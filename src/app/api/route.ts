fdc541e57d3d9a3a6f786c353484cec65285efbd
Evan Tung <dragonejt@gmail.com>
2025-09-17 12:52:50 -0400

    Add Cloudflare API integration for transcript processing

---
+++ src/app/api/route.ts
@@ -0,0 +1,27 @@
+export async function POST(request: Request) {
+    const transcript = await request.text();
+
+    try {
+        const llmResponse = await fetch(`https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/v1/responses`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
+            },
+            body: JSON.stringify({
+                "model": "@cf/openai/gpt-oss-120b",
+                "input": `Identify the last name of each speaker (currently labeled Speaker #) based on the text in this transcript.
+            Do not provide any details other than the name.
+            ${transcript}`
+
+            })
+        })
+        const json = await llmResponse.json()
+        console.log(json)
+
+        return Response.json(json)
+    } catch (error) {
+        console.error(error)
+    }
+
+
+}
